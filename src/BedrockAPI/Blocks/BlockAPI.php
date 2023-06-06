99da40fb189613db80b92d1e508272c8a063c0c8
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-06-06 16:05:43 +0530

    update BlockAPI.php

--- src/BedrockAPI/Blocks/BlockAPI.php
+++ src/BedrockAPI/Blocks/BlockAPI.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace BedrockAPI\Blocks;
+
+use pocketmine\block\VanillaBlocks;
+
+class BlockAPI {
+
+    public static function getBlock(string $BlockName) {
+        $BlockName = strtoupper($BlockName);
+        if (!is_string($BlockName)) {
+            return;
+        }
+        return VanillaBlocks::$BlockName();
+    }
+    //getBlockItem("BlockName???", 1);
+    public static function getBlockItem(string $BlockName, int $count = 1) {
+        if (!is_string($BlockName)) {
+            return;
+        }
+        if (!is_int($count)) {
+            return;
+        }
+        $BlockName = strtoupper($BlockName);
+        return VanillaBlocks::$BlockName()->asItem()->setCount($count);
+    }
+}
