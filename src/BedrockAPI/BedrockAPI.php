70b1d68f619cbb69d89b9d2267eea7e1e80b87c5
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-06-06 16:06:26 +0530

    update BedrockAPI.php

--- src/BedrockAPI/BedrockAPI.php
+++ src/BedrockAPI/BedrockAPI.php
@@ -1,1 +1,36 @@
+<?php
 
+declare(strict_types=1);
+
+namespace BedrockAPI;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use BedrockAPI\Blocks\BlockAPI;
+
+class BedrockAPI extends PluginBase{
+    
+    private static $instance;
+   
+   // onloading plugin...
+    public function onLoad(): void {
+        self::$instance = $this;
+    }
+    
+	public function onEnable() : void{
+		$this->getLogger()->info("§aEnabled BedrockAPI!"); // dont mind i used § symbol for colour instead using TextFormat..
+	}
+
+	public function onDisable() : void{
+		$this->getLogger()->info("§cDisabled BedrockAPI!");
+	}
+	public static function BlockAPI(): BlockAPI {
+        return new BlockAPI();
+    }
+	// For Internal Use Only!
+    public static function getInstance(): self
+    {
+        return self::$instance;
+    }
+}
