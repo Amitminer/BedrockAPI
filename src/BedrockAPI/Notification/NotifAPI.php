bb3fd88d0d5a2cba3eda04ff0bada31e5c389dfb
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-06-20 14:21:47 +0530

    Add files via upload

--- src/BedrockAPI/Notification/NotifAPI.php
+++ src/BedrockAPI/Notification/NotifAPI.php
@@ -1,1 +1,43 @@
+<?php
 
+namespace BedrockAPI\Notification;
+
+use BedrockAPI\BedrockAPI;
+use pocketmine\player\Player;
+use pocketmine\network\mcpe\protocol\ToastRequestPacket;
+
+class NotifAPI {
+
+    public function __construct() {
+        // nothing!
+    }
+
+    public function say(Player $player, string $msg): void {
+        // NotifAPI::say($player, "hellow")
+        if ($player instanceof Player) {
+            $player->sendMessage($msg);
+        } else {
+            return;
+        }
+    }
+
+    public static function toast(Player $player, string $title, string $msg): void {
+        // NotifAPI::toast($player,"test_server","yooo");
+        if (empty($title) || empty($msg)) {
+            return;
+        }
+        if ($player instanceof Player) {
+            return;
+        }
+        $toastpacket = ToastRequestPacket::create($title, $msg);
+        $player->getNetworkSession()->sendDataPacket($toastpacket);
+    }
+    public function popup(Player $player, string $msg): void {
+        // NotifAPI::popup($player,"hii dude");
+        if (empty($msg) || !$player instanceof Player) {
+            return;
+        }
+        $player->sendPopup($msg);
+    }
+    // TODO: Implementing Recipe Toast!
+}
