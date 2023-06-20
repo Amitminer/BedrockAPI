51fe3eb2617dfaf2a9d15852b4d6f21edd607e5d
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-06-20 14:23:05 +0530

    Add files via upload

--- src/BedrockAPI/Command/CommandAPI.php
+++ src/BedrockAPI/Command/CommandAPI.php
@@ -1,1 +1,25 @@
+<?php
 
+namespace BedrockAPI\command;
+
+use BedrockAPI\BedrockAPI;
+
+class CommandAPI {
+
+    public function __construct() {
+        // nothing!
+    }
+
+    public static function unregister(string $cmdName): bool {
+        // CommandAPI::unregister("cmdname");
+        if (!empty($cmdName)) {
+            $commandMap = BedrockAPI::getInstance()->getServer()->getCommandMap();
+            $commandMap->unregister($commandMap->getCommand($cmdName));
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // todo:
+    //register & blocker 
+}
