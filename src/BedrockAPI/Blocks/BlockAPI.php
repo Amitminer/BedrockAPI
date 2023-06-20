904b9065c14ab4724ed2978b2a7bcab3d8ddff8c
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-06-20 14:16:40 +0530

    Add files via upload

--- src/BedrockAPI/Blocks/BlockAPI.php
+++ src/BedrockAPI/Blocks/BlockAPI.php
@@ -3,25 +3,57 @@
 namespace BedrockAPI\Blocks;
 
 use pocketmine\block\VanillaBlocks;
+use pocketmine\block\Block;
+use pocketmine\item\Item;
+use pocketmine\item\LegacyStringToItemParser;
+use pocketmine\item\LegacyStringToItemParserException;
+use pocketmine\item\StringToItemParser;
 
 class BlockAPI {
 
-    public static function getBlock(string $BlockName) {
-        $BlockName = strtoupper($BlockName);
-        if (!is_string($BlockName)) {
-            return;
+    public static function getBlockTypeIds(Block $block): int {
+        // Get the type ID of the block, which can be negative or positive
+        $blockType = $block->getTypeId();
+        if ($blockType < 0) {
+            $blockId = -$blockType;
+        } else {
+            $blockId = $blockType;
         }
-        return VanillaBlocks::$BlockName();
+        return $blockId;
     }
-    //getBlockItem("BlockName???", 1);
-    public static function getBlockItem(string $BlockName, int $count = 1) {
-        if (!is_string($BlockName)) {
-            return;
+
+    public static function stringToBlock(string $input): ?Block {
+        // Convert a string into a Block item (e.g., "minecraft:dirt", "wooden_sword")
+        $string = strtolower(str_replace([' ', 'minecraft:'], ['_', ''], trim($input)));
+        try {
+            $block = StringToItemParser::getInstance()->parse($string) ?? LegacyStringToItemParser::getInstance()->parse($string);
+        } catch (LegacyStringToItemParserException $e) {
+            return null;
         }
-        if (!is_int($count)) {
-            return;
+        return $block;
+    }
+
+    public static function getBlock(string $blockName): ?Block {
+        // Get a block by its name (e.g., getBlock("dirt"))
+        // Remember that you can't use item names as blocks (e.g., if you try to use "diamond_block", it will throw an error)
+        $blockName = strtoupper($blockName);
+        if (!is_string($blockName)) {
+            return null;
         }
-        $BlockName = strtoupper($BlockName);
-        return VanillaBlocks::$BlockName()->asItem()->setCount($count);
+        return VanillaBlocks::$blockName();
+    }
+
+    public static function getBlockItem(string $blockName, int $count = 1): ?Item {
+        // Get a block as an Item with the given count
+        // If count is null, it will default to 1
+        if (!is_string($blockName) || !is_int($count)) {
+            return null;
+        }
+        $blockName = strtoupper($blockName);
+        $block = self::getBlock($blockName);
+        if ($block === null) {
+            return null;
+        }
+        return $block->asItem()->setCount($count);
     }
 }
