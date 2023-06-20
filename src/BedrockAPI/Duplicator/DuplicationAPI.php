ed4153794d4198e1a4cc5161917f48cbaecc898a
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-06-20 14:22:43 +0530

    Add files via upload

--- src/BedrockAPI/Duplicator/DuplicationAPI.php
+++ src/BedrockAPI/Duplicator/DuplicationAPI.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace BedrockAPI\Duplicator;
+
+use pocketmine\item\Item;
+use pocketmine\item\VanillaItems;
+use pocketmine\player\Player;
+
+class DuplicationAPI {
+
+    public static function dupehand(Player $player, int $count = 2): void{
+        $item = $player->getInventory()->getItemInHand();
+        $itemName = $item->getName();
+        if (!$item->isNull()) {
+            $cloned = $item->setCount($count);
+            $player->getInventory()->addItem($cloned);
+            $player->sendMessage("§aSuccessfully cloned {$itemName} ");
+        } else {
+            $player->sendMessage("§cYou can't clone AIR");
+        }
+    }
+}
