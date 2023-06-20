8c47b79a9d331124328adf581efbf13a9dc096f1
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-06-20 14:24:10 +0530

    Add files via upload

--- src/BedrockAPI/Items/ItemsAPI.php
+++ src/BedrockAPI/Items/ItemsAPI.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace BedrockAPI\Items;
+
+use pocketmine\item\VanillaItems;
+
+class ItemsAPI {
+
+    public static function getItem(string $ItemName) {
+        $ItemName = strtoupper($ItemName);
+        if (!is_string($ItemName)) {
+            return;
+        }
+        return VanillaItems::$ItemName();
+    }
+}
